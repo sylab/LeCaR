9d0ce3a1ebc26705a4e85c3bb034d46835393c6f
Systems Research Laboratory (SyLab), FIU <63368629+sylab@users.noreply.github.com>
2020-06-03 19:50:33 -0400

    Update get_algorithm

--- code/alg/get_algorithm.py
+++ code/alg/get_algorithm.py
@@ -1,8 +1,17 @@
+from .lru import LRU
+from .lfu import LFU
+from .arc import ARC
 from .lecar import LeCaR
 
 def get_algorithm(alg_name):
     alg_name = alg_name.lower()
 
+    if alg_name == 'lru':
+        return LRU
+    if alg_name == 'lfu':
+        return LFU
+    if alg_name == 'arc':
+        return ARC
     if alg_name == 'lecar':
         return LeCaR
     return None
